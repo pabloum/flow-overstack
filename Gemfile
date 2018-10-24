bfe72223f634ad53c7ce4b0ebdbebe5e0729d5cc
Pablo <pablo.um00@gmail.com>
2018-10-24 11:11:44 -0500

    Allows to creatre users

--- Gemfile
+++ Gemfile
@@ -22,10 +22,10 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+# Use ActiveModel has_secure_password
+gem 'bcrypt', '~> 3.1.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 
 gem 'bootstrap', '~> 4.1'
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_213626) do
+ActiveRecord::Schema.define(version: 2018_10_24_155244) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2018_10_23_213626) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", limit: 100
+    t.string "password_digest"
+    t.string "name", limit: 100
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
