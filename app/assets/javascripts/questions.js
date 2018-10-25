e55d0978213500eda1eed2929dc6202cb0e279d5
Pablo <pablo.um00@gmail.com>
2018-10-24 22:32:55 -0500

    Edits toggle comments button

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -2,8 +2,12 @@
  // All this logic will automatically be available in application.js.
  // You can use CoffeeScript in this file: http://coffeescript.org/
 
-$(document).ready(function() {
-  $(".toggle-comment-option ").on('click', function() {
-    $(this).parent().parent().find(".answer-comments").toggle();
-  });
-});
+ // $(document).ready(function() {
+
+document.addEventListener("turbolinks:load", function() {
+  // $(window).load(function() {
+    $(".toggle-comment-option ").on('click', function() {
+      $(this).parent().parent().find(".comments").toggle();
+    });
+  // });
+})
